ce8d9fba6c4dcbee4153cffcf016e7be51ec87e2
Kishan Patel <kishanpatel@dhcp-wifi-8021x-155-41-103-8.bu.edu>
2016-11-12 12:04:15 -0500

    put images in assets, started creating table of missions

---
+++ GooseBye/GooseBye/MissionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MissionTableViewCell.swift
+//  GooseBye
+//
+//  Created by Kishan Patel on 11/11/16.
+//  Copyright © 2016 Kishan Patel. All rights reserved.
+//
+
+import UIKit
+
+class MissionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
