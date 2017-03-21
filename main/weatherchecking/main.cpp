677b07f6500ec01dfd46d83977c1bc118e8819d2
Sharar Arzuk Rahman <sharar.rahman@gmail.com>
2017-03-21 14:51:43 -0400

    Fixed weather checking code

--- main/weatherchecking/main.cpp
+++ main/weatherchecking/main.cpp
@@ -38,3 +38,27 @@ int main()
 	if (isSafeToFly) cout << "It's safe to fly" << endl;
 	return 0;
 }
+
+
+bool weatherIsGood(string latitude, string longitude) {
+	string darksky = "https://api.darksky.net/forecast/d5149dd136c55362108974d2d4a41e96/";
+	
+	darksky += latitude + "," + longitude;
+	string todownload = "wget -O test.json -q " + darksky;
+	const char * c = todownload.c_str();
+	system(c);
+	ifstream i("test.json");
+	Json::Reader reader;
+	Json::Value root;
+	reader.parse(i,root);
+	float precipIntensity = root["currently"]["precipIntensity"].asFloat();
+	float precipProbability = root["currently"]["precipProbability"].asFloat();
+
+	bool isSafeToFly = true;
+	if ((precipIntensity > 0) || (precipProbability > 0)) 
+		isSafeToFly = false;
+
+	cout << precipIntensity << " " << precipProbability << endl;
+	if (isSafeToFly) cout << "It's safe to fly" << endl;
+	return 0;
+}
